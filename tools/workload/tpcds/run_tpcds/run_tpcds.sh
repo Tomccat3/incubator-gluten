5bc6175ee2b9597f01811a569c2b4b1dd694de16
Guohui Cai <guohui.cai@transsion.com>
2024-12-04 15:24:04 +0800

    修复hashAggregateExec异常

--- tools/workload/tpcds/run_tpcds/run_tpcds.sh
+++ tools/workload/tpcds/run_tpcds/run_tpcds.sh
@@ -22,12 +22,12 @@ cat tpcds_parquet.scala | ${SPARK_HOME}/bin/spark-shell \
   --conf spark.driver.extraClassPath=${GLUTEN_JAR} \
   --conf spark.executor.extraClassPath=${GLUTEN_JAR} \
   --conf spark.memory.offHeap.enabled=true \
-  --conf spark.memory.offHeap.size=18g \
+  --conf spark.memory.offHeap.size=2g \
   --conf spark.gluten.sql.columnar.forceShuffledHashJoin=true \
   --conf spark.shuffle.manager=org.apache.spark.shuffle.sort.ColumnarShuffleManager \
-  --num-executors 2 \
-  --executor-cores 1 \
-  --driver-memory 4g \
+  --num-executors 3 \
+  --executor-cores 3 \
+  --driver-memory 2g \
   --executor-memory 3g \
   --conf spark.executor.memoryOverhead=2g \
   --conf spark.driver.maxResultSize=2g \
