6d86c802d60ed3663faabff8332fd0920a81f507
Guohui Cai <guohui.cai@transsion.com>
2024-12-09 14:26:35 +0800

    修改libelf地址

--- dev/vcpkg/ports/libelf/portfile.cmake
+++ dev/vcpkg/ports/libelf/portfile.cmake
@@ -1,5 +1,6 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://ftp.osuosl.org/pub/blfs/conglomeration/libelf/libelf-0.8.13.tar.gz"
+ # transsion
+    URLS "https://repository.timesys.com/buildsources/l/libelf/libelf-0.8.13/libelf-0.8.13.tar.gz"
     FILENAME "libelf-0.8.13.tar.gz"
     SHA512 d2a4ea8ccc0bbfecac38fa20fbd96aefa8e86f8af38691fb6991cd9c5a03f587475ecc2365fc89a4954c11a679d93460ee9a5890693112f6133719af3e6582fe
 )
