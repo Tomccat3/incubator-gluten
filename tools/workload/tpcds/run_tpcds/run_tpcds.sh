301198e8ff72b0e3303b52c1495619427e9ae0db
Guohui Cai <guohui.cai@transsion.com>
2024-12-12 10:59:00 +0800

    修改benchmark配置

--- tools/workload/tpcds/run_tpcds/run_tpcds.sh
+++ tools/workload/tpcds/run_tpcds/run_tpcds.sh
@@ -24,7 +24,7 @@ cat tpcds_parquet.scala | ${SPARK_HOME}/bin/spark-shell \
   --conf spark.memory.offHeap.size=2g \
   --conf spark.gluten.sql.columnar.forceShuffledHashJoin=true \
   --conf spark.shuffle.manager=org.apache.spark.shuffle.sort.ColumnarShuffleManager \
-  --num-executors 4 \
+  --num-executors 2 \
   --executor-cores 1 \
   --driver-memory 2g \
   --executor-memory 2g \
